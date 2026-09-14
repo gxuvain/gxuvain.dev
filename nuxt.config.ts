47bca7f4a8cf34e9da33dd753110ebddd3ab5ab0
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-09-14 23:43:34 +0200

    chore: fix deployment config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,27 @@ import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
+  nitro: {
+    preset: "cloudflare_module",
+    cloudflare: {
+      deployConfig: true,
+      wrangler: {
+        d1_databases: [
+          {
+            binding: "DB",
+            database_name: "gxuvain-dev",
+            database_id: "8953eab1-a884-4749-b524-7eddfbc9eb0c",
+          },
+        ],
+      },
+    },
+  },
+  content: {
+    database: {
+      type: "d1",
+      bindingName: "DB",
+    },
+  },
   modules: ["@nuxt/eslint", "@nuxt/content", "@nuxt/icon", "@nuxtjs/color-mode", "nuxt-toc"],
   css: ["~/assets/css/main.css"],
   colorMode: { fallback: "dark" },
